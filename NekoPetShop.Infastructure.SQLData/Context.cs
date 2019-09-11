77ea0db089493be75736ed122044ce24eab1056f
kidagine <36992613+kidagine@users.noreply.github.com>
2019-09-11 12:46:02 +0200

    +Added initial setup for the entity framework.

---
+++ NekoPetShop.Infastructure.SQLData/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NekoPetShop.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NekoPetShop.Infastructure.SQLData
+{
+    class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> opt): base(opt)
+        {
+
+        }
+
+        public DbSet<Owner> owners { get; set; }
+        public DbSet<Pet> pets { get; set; }
+    }
+}
---
+++ NekoPetShop.Infastructure.SQLData/Repositories/PetRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using NekoPetShop.Core.Entity;
+using NekoPetShop.Core.DomainService;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace NekoPetShop.Infastructure.SQLData.Repositories
+{
+    class PetRepository : IPetRepository
+    {
+        private Context context;
+
+        public PetRepository(Context context)
+        {
+            this.context = context;
+        }
+        public Pet CreatePet(Pet pet)
+        {
+            context.Add(pet);
+            context.SaveChanges();
+            return pet;
+        }
+
+        public Pet DeletePet(int id)
+        {
+            var entityEntry = context.Remove(new Pet() { Id = id });
+            context.SaveChanges();
+            return entityEntry.Entity;
+        }
+
+        public IEnumerable<Pet> GetPets()
+        {
+            return context.pets.ToList();
+        }
+
+        public Pet UpdatePet(int id, Pet pet)
+        {
+            var entityEntry = context.Update(pet);
+            context.SaveChanges();
+            return entityEntry.Entity;
+        }
+    }
+}
