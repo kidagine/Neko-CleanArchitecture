13b98ec68bb934751b36ccde0ea76cbc5742743e
kidagine <36992613+kidagine@users.noreply.github.com>
2019-11-15 11:07:58 +0100

    Revert "+Big update for frontend angular, to catch up with everything done so far in the backend."
    This reverts commit 7b76dce086d2a8cc422e822f87ad9e2ef5f4e8ea.

--- NekoPetShop.Angular/neko-petshop/src/app/shared/models/petColor.model.ts
+++
@@ -1,9 +0,0 @@
-import { Color } from 'src/app/colors/shared/color.model';
-import { Pet } from 'src/app/pets/shared/pet.model';
-
-export class PetColor {
-  petId: number;
-  pet: Pet;
-  colorId: number;
-  color: Color;
-}
--- NekoPetShop.Angular/neko-petshop/src/app/shared/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-  id: number;
-  username: string;
-  passwordHash: Uint8Array
-  passwordSalt: Uint8Array
-  isAdmin: boolean;
-}
